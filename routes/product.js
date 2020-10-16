2faad5c02dc9509aae66cfa67e7de9653a39fcf9
omaryakoubi <yakoubiomar2001@gmail.com>
2020-10-16 14:01:48 +0100

    product methods done

--- routes/product.js
+++ routes/product.js
@@ -1,6 +1,16 @@
 const router = require("express").Router();
 const product = require("../models/products");
 
+router.get("/all-products", async (req, res) => {
+  try {
+    const productList = await product.find({});
+    res.send(productList);
+  } catch (error) {
+    console.log(error);
+    res.send("error");
+  }
+});
+
 router.post("/new-product", async (req, res) => {
   try {
     await new product({
@@ -16,6 +26,15 @@ router.post("/new-product", async (req, res) => {
   }
 });
 
+router.put("/edit-product/:id", async (req, res) => {
+  try {
+    await product.findByIdAndUpdate(req.params.id, req.body);
+    res.status(200).send("product edit successfully");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 router.delete("/delete-product/:id", async (req, res) => {
   try {
     await product.findByIdAndDelete(req.params.id);
@@ -23,15 +42,6 @@ router.delete("/delete-product/:id", async (req, res) => {
   } catch (error) {
     console.log(error);
     res.status(404).send("can't delete product not found");
-  }
-});
-
-router.put("/edit-product/:id", async (req,res) => {
-  try {
-    await product.findByIdAndUpdate(req.params.id,req.body);
-    res.send(req.body)
-  } catch(error) {
-    console.log(error)
   }
 });
 
