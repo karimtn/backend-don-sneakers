b84df4f37414f095c7fe051e9dad7d6444ff7869
omaryakoubi <yakoubiomar2001@gmail.com>
2020-10-16 11:18:50 +0100

    db connection done

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 const express = require("express");
+const mongoose = require("mongoose")
 const bodyParser = require("body-parser");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const app = express();
+require("dotenv").config();
+
 
 // # MIDDLEWARES # 
 app.use(bodyParser.json());
 app.use(morgan("combined"));
 app.use(helmet);
 
+// #DATABASE CONNECTION#
+const DB_URL = process.env.MONGODB_URL;
+
+mongoose.connect(DB_URL, {useNewUrlParser: true, useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connected");
+});
+
 // # SERVER PORT#
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 
 app.listen(PORT, () => {
