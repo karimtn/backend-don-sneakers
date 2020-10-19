0fa7b54e1ebb617e79658c93028a3a322889eb69
omaryakoubi <yakoubiomar2001@gmail.com>
2020-10-19 15:32:19 +0100

    refactoring

--- api/admin/admin-authentication.js
+++ api/admin/admin-authentication.js
@@ -1,24 +1,32 @@
 const router = require("express").Router();
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 const admin = require("../../models/admin");
 require("dotenv").config();
 
 router.post("/register", async (req, res) => {
   try {
-    let { firstName, lastName, email, password, passwordConfirmation } = req.body;
+    let {
+      firstName,
+      lastName,
+      email,
+      password,
+      passwordConfirmation,
+    } = req.body;
     const emailRegistered = await admin.findOne({ email });
-   
+
     if (emailRegistered) {
       res.send("your email already exist");
     }
 
-    if (password.length < 6) {
+    if (password.length > 6) {
       res.send("your password length should be more than 6 please ");
     }
 
     if (password !== passwordConfirmation) {
-      res.send("your password and your password confirmation are not equal try again");
+      res.send(
+        "your password and your password confirmation are not equal try again"
+      );
     } else {
       const newAdmin = new admin({
         firstName,
@@ -29,7 +37,7 @@ router.post("/register", async (req, res) => {
         resetPasswordExpires: 0,
       });
 
-      bcrypt.hash(newAdmin.password, 10, async (err,hash) => {
+      bcrypt.hash(newAdmin.password, 10, async (err, hash) => {
         if (err) {
           console.log(err);
         } else {
@@ -39,34 +47,36 @@ router.post("/register", async (req, res) => {
       });
       res.status(201).send("new admin created");
     }
-
   } catch (error) {
     console.log(error);
-    res.send(error)
+    res.send(error);
   }
 });
 
 router.post("/login", async (req, res) => {
-  const { _id, email, password } = req.body;
-  const adminAccount = await admin.findOne({ email });
-  if (adminAccount) {
-     bcrypt.compare(password, adminAccount.password, (err, res) => {
-       if (err) {
-         console.log(err);
-         res.send("Auth failed");
-       }
+  try {
+    const { _id, email, password } = req.body;
+    const adminInfo = await admin.findOne({ email });
+    if (adminInfo) {
+      bcrypt.compare(password, adminInfo.password, (err, result) => {
+        if (err) {
+          res.send("Auth failed");
+        }
 
-       if (res) {
-        const token = jwt.sign({email,_id},process.env.JWT_KEY,{expiresIn:"1h"})
-        console.log(token)
-      }
-      
-    });
-  } else {
-    console.log('not exist')
-    res.send("admin doesent exist");
+        if (result) {
+          const token = jwt.sign({ email, _id }, process.env.JWT_KEY, {
+            expiresIn: "1h",
+          });
+          res.json(token);
+        }
+      });
+    } else {
+      res.send("admin by this email doesent exist");
+    }
+  } catch (error) {
+    res.send(error);
+    console.log(error);
   }
 });
-
 
 module.exports = router;
--- api/user/user-authentication.js
+++ api/user/user-authentication.js
@@ -17,7 +17,7 @@ router.post("/register", async (req, res) => {
       res.send("your phone number is already linked to another account");
     }
 
-    if (password.length < 6) {
+    if (password.length > 6) {
       res.send("your password length should be more than 6 please ");
     }
 
@@ -54,5 +54,30 @@ router.post("/register", async (req, res) => {
   }
 });
 
+router.post("/login", async (req, res) => {
+  try {
+    const { _id, email, password } = req.body;
+    const userInfo = await user.findOne({ email });
+    if (userInfo) {
+      bcrypt.compare(password, userInfo.password, (err, result) => {
+        if (err) {
+          res.send("Auth failed");
+        }
+
+        if (result) {
+          const token = jwt.sign({ email, _id }, process.env.JWT_KEY, {
+            expiresIn: "1h",
+          });
+          res.json(token);
+        }
+      });
+    } else {
+      res.send("user by this email doesent exist");
+    }
+  } catch (error) {
+    res.send(error);
+    console.log(error);
+  }
+});
+
 module.exports = router;
-658921623
