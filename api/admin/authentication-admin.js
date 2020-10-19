6ced5aaa4a3b4b0696f23cb3a5a4a89490e43c40
omaryakoubi <yakoubiomar2001@gmail.com>
2020-10-19 10:36:51 +0100

    refactoring auth admin done

--- api/admin/authentication-admin.js
+++ api/admin/authentication-admin.js
@@ -1,15 +1,23 @@
 const router = require("express").Router();
-const bcrypt = require('bcrypt')
+const bcrypt = require("bcrypt");
 const admin = require("../../models/admin");
 
 router.post("/register", async (req, res) => {
   try {
     let { firstName, lastName, email, password, passwordConfirmation } = req.body;
     const emailRegistered = await admin.findOne({ email });
-    console.log('first log',password)
-    
-    if (!emailRegistered && password.length >= 6 && password === passwordConfirmation) {
 
+    if (emailRegistered) {
+      res.send("your email already exist");
+    }
+
+    if (password.length < 6) {
+      res.send("your password length should be more than 6 please ");
+    }
+
+    if (password !== passwordConfirmation) {
+      res.send("your password and your password confirmation are not equal try again");
+    } else {
       const newAdmin = new admin({
         firstName,
         lastName,
@@ -17,22 +25,21 @@ router.post("/register", async (req, res) => {
         password,
         resetPasswordToken: "",
         resetPasswordExpires: 0,
-      })
-      
-      bcrypt.hash(newAdmin.password, 10, (err,hash) => {
-        if (err) {
-          console.log(err)
-        } else {
-          newAdmin.password = hash
-          }
-      })
+      });
 
-      await newAdmin.save()
-      res.status(201).send('new admin created')
+      bcrypt.hash(newAdmin.password, 10, (err, hash) => {
+        if (err) {
+          console.log(err);
+        } else {
+          newAdmin.password = hash;
+        }
+      });
+
+      await newAdmin.save();
+      res.status(201).send("new admin created");
     }
-  } catch (error) {
-    console.log(error);
-  }
+  } catch (error) {}
+  console.log(error);
 });
 
 module.exports = router;
