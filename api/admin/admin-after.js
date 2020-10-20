619027911975e289f4678b051fcd74138dce79ac
omaryakoubi <yakoubiomar2001@gmail.com>
2020-10-20 16:14:58 +0100

    naming without overwriting pdf file done

--- api/admin/admin-after.js
+++ api/admin/admin-after.js
@@ -1,73 +1,78 @@
 const router = require("express").Router();
-const easyinvoice = require('easyinvoice');
+const easyinvoice = require("easyinvoice");
 const user = require("../../models/users");
 const product = require("../../models/products");
 const selledProduct = require("../../models/selledProduct");
-const fs = require("fs")
+const fs = require("fs");
+let number = 0
 
 router.post("/after/:user_id/:product_id", async (req, res) => {
   try {
-    const {user_id, product_id} = req.params 
+    const { user_id, product_id } = req.params;
     const userInfo = await user.findById(user_id);
     const productInfo = await product.findById(product_id);
     let today = new Date();
-    const dd = String(today. getDate()). padStart(2, '0');
-    const mm = String(today. getMonth() + 1). padStart(2, '0');
-    const yyyy = today. getFullYear();
-    today = ( mm + '-' + dd + '-' + yyyy );
+    const dd = String(today.getDate()).padStart(2, "0");
+    const mm = String(today.getMonth() + 1).padStart(2, "0");
+    const yyyy = today.getFullYear();
+    today = mm + "-" + dd + "-" + yyyy;
     let data = {
-        "currency": "EUR",
-        "taxNotation": "vat",
-        "marginTop": 25,
-        "marginRight": 25,
-        "marginLeft": 25,
-        "marginBottom": 25,
-        "logo": "https://www.snupps.com/api/2.10/users/355355/avatar/355355.400.jpg",
-        
-        "sender": {
-            "company": "Sneaker Don",
-            "address": "....",
-            "zip": "45484aa",
-            "city": "Lyon",
-            "country": "France"
-        
+      currency: "EUR",
+      taxNotation: "vat",
+      marginTop: 25,
+      marginRight: 25,
+      marginLeft: 25,
+      marginBottom: 25,
+      logo:
+        "https://www.snupps.com/api/2.10/users/355355/avatar/355355.400.jpg",
+
+      sender: {
+        company: "Sneaker Don",
+        address: "....",
+        zip: "45484aa",
+        city: "Lyon",
+        country: "France",
+      },
+      client: {
+        company: `${userInfo.firstName} ${userInfo.lastName}`.toUpperCase(),
+        address: `${userInfo.address}`,
+        zip: `${userInfo.postalCode}`,
+        city: `${userInfo.city}`,
+        country: `${userInfo.country}`,
+      },
+      invoiceNumber: "",
+      invoiceDate: today,
+      products: [
+        {
+          quantity: req.body.quantity,
+          description: productInfo.name,
+          tax: productInfo.tax,
+          price: productInfo.price,
         },
-        "client": {
-               "company": `${userInfo.firstName} ${userInfo.lastName}`.toUpperCase(),
-               "address": `${userInfo.address}`,
-               "zip": `${userInfo.postalCode}`,
-               "city": `${userInfo.city}`,
-               "country": `${userInfo.country}`
-        },
-        "invoiceNumber": "",
-        "invoiceDate": today,
-        "products": [
-            {
-                "quantity": req.body.quantity,
-                "description": productInfo.name,
-                "tax": productInfo.tax,
-                "price": productInfo.price
-            },
-        ],
+      ],
     };
+    
+    const result = await easyinvoice.createInvoice(data);
+    await fs.writeFileSync(
+       `#${number} ${userInfo.firstName} ${userInfo.lastName} ${today}.pdf`,
+      result.pdf,
+      "base64"
+    );
+    number += 1
 
-    const result = await easyinvoice.createInvoice(data);    
-    await fs.writeFileSync(`${userInfo.name}  ${today}.pdf`, result.pdf, 'base64');               
-
-      await new selledProduct({
+    await new selledProduct({
       userMail: userInfo.email,
       productName: productInfo.name,
       price: productInfo.price,
       quantity: req.body.quantity,
       total: productInfo.price * req.body.quantity,
     }).save();
-    
-    res.json('shipping info saved in the db')
 
+    res.json("shipping info saved in the db");
   } catch (error) {
     res.send(error);
     console.log(error);
   }
 });
 
-module.exports = router
+module.exports = router;
