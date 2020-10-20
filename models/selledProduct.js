0a1f0c76db4e2d6facfa965b36192f29e6801339
omaryakoubi <yakoubiomar2001@gmail.com>
2020-10-20 10:24:12 +0100

    new schema added

---
+++ models/selledProduct.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const selledProductSchema = new Schema({
+  userMail: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  total: {
+    type: Number,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("selledProduct",selledProductSchema)
