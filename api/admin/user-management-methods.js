03b4ba0757d5deaef972c94c44cdd74684448788
omaryakoubi <yakoubiomar2001@gmail.com>
2020-11-02 12:07:20 +0100

    struggling with a bug

--- api/admin/user-management-methods.js
+++ api/admin/user-management-methods.js
@@ -2,18 +2,20 @@ const router = require("express").Router();
 const checkAuth = require("../routes-protector")
 const user = require("../../models/users");
 
-// # DELETE A USER #
 
-router.get("user-list", checkAuth, async (req, res) => {
+router.get("user-list", async (req, res) => {
   try{
     const userList = await user.find({})
-    res.json({message: "userList", userList})
+    console.log(userList)
+    res.send(userList)
   } catch(error) {
-    res.json(error)
     console.log(error)
+    res.json(error)
   }
 })
-router.delete("/delete-user/:id", checkAuth, async (req, res) => {
+
+// # DELETE A USER #
+router.delete("/delete-user/:id", async (req, res) => {
   try {
     await user.findByIdAndDelete(req.params.id);
     res.status(204).send("user deleted successfully");
